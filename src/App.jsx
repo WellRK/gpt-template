bd2a35af447bbbf0a0b542c105c18e88656e8cf1
WellRK <wellrokembach@gmail.com>
2024-03-09 16:02:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return <div className="container mx-auto h-screen flex flex-row">
+    <div className="felx-grow">
+    
+    </div>
+    
+    
+    
+    <div>
+
+    </div>
+
+  </div>
+  
+}
+
+export default App
